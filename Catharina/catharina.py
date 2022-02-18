54f6911d94ad259c284eef3ace5d7f8968272819
KattenCatharina <catharina.na21r@hvilanutbildning.se>
2022-02-18 12:23:30 +0100

    hellu

--- Catharina/catharina.py
+++ Catharina/catharina.py
@@ -1,9 +1,24 @@
+
+#Medelvärdet av tal i given lista 
 def medelvärde(x):
-    avg = sum (x) / len (lista)
+    avg = sum (x) / len (x)
+    return avg
 lista = [1,2,3,4,5]
 medelvärde = medelvärde(lista)
 
-print ("medelvärde av lista", medelvärde)
+print ("medelvärde av lista är", medelvärde)
+
+#variationsbredd av tal i given lista
+lista = [1,2,2,4,6,6,9]
+
+st=len(lista)-1
+vb=lista[st] - lista[0]
+print("Variationsbredden är", (vb))
+
+#
+
+
+
  
 
 
