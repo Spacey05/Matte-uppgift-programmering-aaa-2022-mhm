540c436976317b2ac1e882f45a6a6dc699dcca3b
KattenCatharina <catharina.na21r@hvilanutbildning.se>
2022-02-23 12:26:52 +0100

    ekskjd

--- Catharina/catharina.py
+++ Catharina/catharina.py
@@ -1,24 +1,61 @@
+x = [4,2,7,5,23,16,8,4]
 
 #Medelvärdet av tal i given lista 
-def medelvärde(x):
-    avg = sum (x) / len (x)
+def medelvärde (x):
+    avg = sum(x) / len(x)
     return avg
-lista = [1,2,3,4,5]
-medelvärde = medelvärde(lista)
+
+medelvärde = medelvärde(x)
 
 print ("medelvärde av lista är", medelvärde)
 
-#variationsbredd av tal i given lista
-lista = [1,2,2,4,6,6,9]
+#Standardavvikelse av tal i given lista
+def standardavvikelse (x):
+    n = len(x)
+    medel = sum(x) / n
+    var = sum((x- medel)**2 for x in x) / n
+    standardavvikelse = var ** 0.5
+    return standardavvikelse
 
-st=len(lista)-1
-vb=lista[st] - lista[0]
-print("Variationsbredden är", (vb))
+print("standardavvikelse av lista =", standardavvikelse(x))
 
-#
+#Variationsbredd av tal i given lista
+def variationsbredd (x):
+    storlek = len(x)-1
+    vb = sorted(x)[storlek] - sorted(x)[0]
+    return vb
 
+print("Variationsbredden är", variationsbredd(x))
 
+#Median av tal i given lista
+def median (x):
+    storlek = len(x)
+    med = storlek // 2
+    if storlek % 2:
+        return sorted(x)[med]
+    return sum(sorted(x)[med - 1:med + 1]) / 2
 
+print ("medianen av listan=", median(x))
+
+#Kvartilerna i given lista
+def kvartilun(x):
+    x.sort()
+    return x[0:int(len(x)/2)]
+
+print ("undre kvartilen =", median(kvartilun(x)))
+
+def kvartilup (x):
+    x.sort()
+    return x[int(median(x)):-1]
+
+print ("övre kvartilen =", median(kvartilup(x)))
+    
+def kvartilavstånd (x):
+    up = median(kvartilup(x))
+    un = median(kvartilun(x))
+    return up - un
+
+print ("kvartilavstånd =", kvartilavstånd(x))
  
 
 
